da78432a1076627488179bf28a4d5918e4780a9e
Ivan <ivanmajer011@gmail.com>
2025-11-08 21:46:51 +0100

    init window

--- SFML tutroijal/main.cpp
+++ SFML tutroijal/main.cpp
@@ -1,24 +1,50 @@
 #include <SFML/Graphics.hpp>
-#include <optional>
+#include <SFML/Window.hpp>
+#include <random>
+//#include <SFML/OpenGL.hpp>
+
+using namespace sf;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    
+	RenderWindow window(VideoMode(640, 480), "Game 1", Style::Default);
+    std::random_device rd;
+    std::mt19937 gen(rd()); // Mersenne Twister generator (vrlo dobar i brz)
+
+    // Uniformna raspodela u intervalu [1, 100]
+    std::uniform_int_distribution<> distrib(1, 100);
+
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (event.type)
+            {
+                /*case sf::Event::Resized:
+                    glViewport(0, 0, event.size.width, event.size.height);
+                    break;*/
+				case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape) {
+						window.close();
+                    }
+                    break;
+                case sf::Event::Closed:
+                    window.close();
+					break;
+                default:
+                    break;
+			}
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        window.clear(sf::Color::White);
+
+		//Draw your game objects here
+
+		window.display();
     }
+
 
     return 0;
 }
